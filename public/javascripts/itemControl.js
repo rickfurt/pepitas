0c69636a4f69358c35ea18264d00f9a06e614f73
rickfurt <ricardofurtado2609@gmail.com>
2019-09-01 23:39:29 +1000

    first commit

---
+++ public/javascripts/itemControl.js
@@ -0,0 +1,70 @@
+var full_item='';
+var size;
+var description;
+var quantity;
+var listItem = [];
+
+var addButton = document.getElementsByClassName('addButton');
+for(var i = 0;i<addButton.length;i++){
+    var button = addButton[i]
+    button.addEventListener('click',function(event){
+        var buttonClicked = event.target;
+        // console.log('cliked',buttonClicked);
+        var item = buttonClicked.parentElement;
+        
+        var description = item.getElementsByClassName('item-description')[0].innerText;
+        var smallSize = item.getElementsByClassName('sizes')[0].checked;
+        var largeSize = item.getElementsByClassName('sizes')[1].checked;
+        quantity = item.getElementsByClassName('quantity')[0].value;
+
+        checkSize(smallSize);
+
+        full_item = description + ' | ' +'Size:  ' + size + ' | ' + 'Qty:  ' + quantity;
+        // console.log(full_item);
+        // console.log(description,size);
+        addToPreview(description,size,quantity);
+        addToList(full_item);
+        urlHandle();
+    });
+}
+
+function addToList (full){
+    listItem.push(full);
+    // console.log(listItem);
+}
+
+//Convert and send to whatsapp
+function urlHandle (){    
+    var link = document.getElementById('url-handle');
+    var baseUlr = 'https://api.whatsapp.com/send?phone=61414515970&text=';
+    var lista = listItem.toString();
+    var newLine = '\n';
+    var regex = /[,]/g;
+    var test = lista.replace(regex,newLine);
+    var message = encodeURI(test);
+    var dynamicUrl = baseUlr.concat(message);
+    link.setAttribute("href", dynamicUrl);
+    }
+
+function checkSize(check){
+    if(check){
+        size = 'Small'
+    }else{
+        size = 'Large'
+    }
+}
+
+function addToPreview(description,size,quantity){
+    var node = document.createElement("LI");
+    node.classList.add("list-group-item");
+    var textnode = document.createTextNode('Description: '+ description + ' | ' + 'Size: '+ size + ' | ' + 'Quantity: ' + quantity);
+    node.appendChild(textnode);
+    document.getElementById("preview").appendChild(node);
+}
+
+
+//previewer refresh 2 seconds
+// setInterval(function(){
+//     document.getElementById("preview").innerHTML = itens;
+// },2000);
+
